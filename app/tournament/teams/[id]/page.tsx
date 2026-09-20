4cfb7a2ec4ecf614fca41093064867c66e07c08f
ff2303721-blip <ff2303721@gmail.com>
2026-09-20 07:30:33 +0530

    Fix tournament branding imports

--- app/tournament/teams/[id]/page.tsx
+++ app/tournament/teams/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { TournamentBrand } from "../components/tournament-brand";
+import { TournamentBrand } from "../../components/tournament-brand";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
